978d3c73e366160d012299205c1b20ce0d26fe88
Jason Fennell <jdfennell@gmail.com>
2010-04-13 22:57:45 -0700

    Move global state into an object, clean up names a bit.

--- js/jester.js
+++ js/jester.js
@@ -1,56 +1,30 @@
+var QC = {
+	'comicId': 1600,
 
-/*
-document.onkeydown = function(e) {
-	// 37 is left, 39 is right, 32 is space
-	//alert("Got a keydown event!.  It is: " + e.which);
-	var outdiv = document.getElementById("test-div");
-	outdiv.firstChild.data = e.which;
-}
-*/
+	'url': function() {
+		return "http://questionablecontent.net/comics/" + this.comicId + ".png";
+	},
 
-/*
-document.onkeydown = function(e) {
-	var img = document.getElementById("pic");
-	if (e.which == 37) {
-		img.src = "img/1.jpg";	
-	} else if (e.which == 39) {
-		img.src = "img/2.jpg";
-	} else if (e.which == 32) {
-		img.src = qcUrl(1641);
-	} else {
-		img.src = "img/3.jpg";
+	'next': function() {
+		this.comicId += 1;
+		return this.url();
+	},
+
+	'prev': function() {
+		this.comicId -= 1;
+		return this.url();
 	}
-}
-*/
+};
 
-// QC url
-// http://questionablecontent.net/comics/1641.png
-function qcUrl(n) {
-	return "http://questionablecontent.net/comics/" + n + ".png";
-}
+function makeMover(qc) {
+	return function(e) {
+		var img = document.getElementById("pic");
+		if (e.which == 37) { // Left
+			img.src = qc.prev();
+		} else if (e.which == 39) { // Right
+			img.src = qc.next();
+		}
+	};
+};
 
-var qcStart = 1600;
-
-function qcNextUrl() {
-	qcStart += 1;
-	return qcUrl(qcStart);
-}
-
-function qcPrevUrl() {
-	qcStart -= 1;
-	return qcUrl(qcStart);
-}
-
-// TODO: move to === ?
-function qcMove(e) {
-	var img = document.getElementById("pic");
-	if (e.which == 37) { // Left
-		img.src = qcPrevUrl();
-	} else if (e.which == 39) {
-		img.src = qcNextUrl();
-	} else if (e.which = 32) {
-		img.src = qcNextUrl();
-	}
-}
-
-document.onkeydown = qcMove;
+document.onkeydown = makeMover(QC);
