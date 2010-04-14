d48e969e621dae4d0b86d1514651bc0a958e70a8
Jason Fennell <jdfennell@gmail.com>
2010-04-14 09:06:38 -0700

    Added basic image pre-fetching.

--- js/jester.js
+++ js/jester.js
@@ -4,31 +4,60 @@
  */
 var QC = {
 	'comicId': 1600,
+	'warmWindow': 2,
+	'comicCache': {},
 
-	'imageCache': {},
-
-	'checkCache': function() {
-		var img = this.imageCache[this.comicId];
-		if (img === undefined) {
-			img = new Image()
-			this.imageCache[this.comicId] = img;
-			img.src = this.url();
-		}
-		return img.src;
+	'url': function(n) {
+		// TODO: Add some validation here.
+		return "http://questionablecontent.net/comics/" + n + ".png";
 	},
 
-	'url': function() {
-		return "http://questionablecontent.net/comics/" + this.comicId + ".png";
+	/**
+	 * Cached getter of the comic with id `n`.
+	 */
+	'getComic': function(n) {
+		var comic = this.comicCache[n];
+		if (comic === undefined) {
+			comic = new Image();
+			this.comicCache[n] = comic;
+			comic.src = this.url(n);
+		}
+		return comic;
+	},
+
+	/**
+	 * Hit the getComic function repeatedly to (in the background)
+	 * start image retrievals for a few comics before and after
+	 * the current one.
+	 *
+	 * Should this do getComic(n) first to give it some sort of priority?
+	 * We want the *current* one to load as fast as possible.
+	 *
+	 * NOTE: In standard use case probably want a longer forward
+	 * window than backward window because you are reading the strip forward.
+	 *
+	 * Additionally, might want to do LRU cache or something... need
+	 * to ask jlatt or somebody about how you profile memory useage of
+	 * your js.
+	 */
+	'warmSurrounding': function(n) {
+		for(var i = -this.warmWindow; i <= this.warmWindow; i += 1) {
+			this.getComic(n + i);
+		}
 	},
 
 	'next': function() {
 		this.comicId += 1;
-		return this.checkCache();
+		this.warmSurrounding(this.comicId);
+		var comic = this.getComic(this.comicId);
+		return comic.src;
 	},
 
 	'prev': function() {
 		this.comicId -= 1;
-		return this.checkCache();
+		this.warmSurrounding(this.comicId);
+		var comic = this.getComic(this.comicId);
+		return comic.src;
 	}
 };
 
