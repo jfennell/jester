af863d0bdaae8082b8b888e05a7b86f850ad81da
Jason Fennell <jdfennell@gmail.com>
2011-04-21 23:46:01 -0700

    Greasemonkey script for girl genius

---
+++ greasemonkey/girl_genius_paging.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name           Girl Genius Paging
+// @namespace      http://www.jasonfennell.com
+// @description    Let you navigate forward and back with left and right arrows
+// @include        http://www.girlgeniusonline.com/comic.php?*
+// ==/UserScript==
+
+// Don't run on frames or iframes
+if (window.top != window.self) { return; }
+
+var girlGenius = function() {
+	var prevComicAlt = "The Previous Comic";
+	var nextComicAlt = "The Next Comic";
+
+	var imgs = document.getElementsByTagName("img");
+
+	var nav = {}
+	// Figure out the next/prev urls
+	for (var i = 0; i < imgs.length; ++i) {
+		if (imgs[i].alt === prevComicAlt) {
+			nav["prev"] = imgs[i].parentNode.href;	
+		} else if (imgs[i].alt === nextComicAlt) {
+			nav["next"] = imgs[i].parentNode.href;
+		}
+	}
+
+	//alert("Prev: " + nav["prev"] + "\nNext: " + nav["next"] + "\nlocaction.href: " + location.href);
+
+	// Handle left/right arrows to navigate
+	var handleArrow = function(e) {
+		if (e.which === 37 && nav["prev"] !== undefined) { // Left
+			//alert("Saw left");
+			location.href = nav["prev"];
+		} else if (e.which === 39 && nav["next"] !== undefined) { // Right
+			//alert("Saw right");
+			location.href = nav["next"];
+		}
+	}
+	document.addEventListener("keydown", handleArrow, true);
+}
+
+girlGenius();
